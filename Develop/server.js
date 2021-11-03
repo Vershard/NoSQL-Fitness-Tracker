55523e4e88e5710714a0233e83e5f2c46359031c
Vershard <vershardthomas@gmail.com>
2021-11-03 18:21:16 -0400

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -25,3 +25,19 @@ app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+// set up cluster in MongoDB Atlas.
+// used connection string to connect Atlas to Heroku (see config vars)
+// connected Github to NoSQL Fitness Tracker Repo
+
+// *** When we go to deploy the main branch getting an error.......
+
+//Build main 2c48dd20
+//There was an issue deploying your app. View the build log for details.
+
+//-----> Building on the Heroku-20 stack
+//-----> Determining which buildpack to use for this app
+ //!     No default language could be detected for this app.
+	//		HINT: This occurs when Heroku cannot detect the buildpack to use for this application automatically.
+		//	See https://devcenter.heroku.com/articles/buildpacks
+ // !     Push failed
